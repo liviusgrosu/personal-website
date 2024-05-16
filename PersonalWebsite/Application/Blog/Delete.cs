17d2b5f527ac26655698c42d5ded5832d6ff19b1
liviusgrosu <liviusgrosu@gmail.com>
2024-05-16 14:59:23 -0600

    Added delete blog post delete

--- PersonalWebsite/Application/Blog/Delete.cs
+++ PersonalWebsite/Application/Blog/Delete.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Core;
+using MediatR;
+using Persistence;
 
 namespace Application.Blog
 {
-    internal class Delete
+    public class Delete
     {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var blogPost = await _context.BlogPosts.FindAsync(request.Id);
+
+                if (blogPost == null)
+                {
+                    return Result<Unit>.Failure("Blog post not found");
+                }
+
+                _context.Remove(blogPost);
+
+                var results = await _context.SaveChangesAsync() > 0;
+
+                if (!results)
+                {
+                    return Result<Unit>.Failure("Failed to delete the blog post");
+                }
+
+                return Result<Unit>.Success(Unit.Value);
+            }
+        }
     }
 }

e77af27f9210d9327cc36c6ed5cfd72a06ff042c
liviusgrosu <liviusgrosu@gmail.com>
2024-05-27 16:24:10 -0600

    Injected store and router provider to app

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './App.css'
 import "semantic-ui-css/semantic.min.css";
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes.tsx';
+import { StoreContext, store } from './app/stores/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <RouterProvider router={router} />
+    </StoreContext.Provider>
   </React.StrictMode>,
 )

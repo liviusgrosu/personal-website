dacdcba57e0ee28f41cec28c739b554c8427fee1
liviusgrosu <liviusgrosu@gmail.com>
2024-05-27 22:41:02 -0600

    Added blog posts

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,16 +1,19 @@
 import { createContext, useContext } from "react";
 import AboutStore from "./aboutStore";
 import ProjectStore from "./projectStore";
+import BlogPostStore from "./blogPostStore";
 // Define the store
 interface Store {
     aboutStore: AboutStore,
-    projectStore: ProjectStore
+    projectStore: ProjectStore,
+    blogPostStore: BlogPostStore
 }
 
 // We instantiate our stores here
 export const store: Store = {
     aboutStore: new AboutStore(),
-    projectStore: new ProjectStore()
+    projectStore: new ProjectStore(),
+    blogPostStore: new BlogPostStore()
 }
 
 // We create the react context of the store so its available to use between components

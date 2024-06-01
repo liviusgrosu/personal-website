c91f18a705a7d16d33bb1c6b0836b33bca3cf753
liviusgrosu <liviusgrosu@gmail.com>
2024-06-01 13:50:47 -0600

    Added simple login page when logging in

---
+++ client-app/src/features/LoginForm.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-refresh/only-export-components */
+import { ErrorMessage, Form, Formik } from "formik";
+import { Button, Header, Label } from "semantic-ui-react";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../app/stores/store";
+import MyTextInput from "../app/common/MyTextInput";
+
+export default observer (function LoginForm() {
+    const {loginStore} = useStore();
+    return (
+        <Formik
+            initialValues={{email: '', password: '', error: null}}
+            onSubmit={(values, {setErrors}) => loginStore.login(values).catch(() => 
+                setErrors({error: 'Invalid email or password'}) )}>
+
+            {({handleSubmit, isSubmitting, errors}) => (
+                <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+                    <Header as='h2' content='Login' color="teal" textAlign="center"/>
+                    <MyTextInput placeholder="Email" name='email'/>
+                    <MyTextInput placeholder="Password" name='password' type='password'/>
+                    <ErrorMessage
+                        name='error' render={() => <Label style={{marginBottom: 10 }} basic color='red' content={errors.error}/>}
+                    />
+                    <Button loading={isSubmitting} positive content='Login' type="submit" fluids/>
+                </Form>
+            )}
+        </Formik>
+    )
+})

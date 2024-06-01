7a244a5eaa284b51c31166a9bd505d1b408a9260
liviusgrosu <liviusgrosu@gmail.com>
2024-06-01 13:50:20 -0600

    Added store for login and common

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -2,18 +2,25 @@ import { createContext, useContext } from "react";
 import AboutStore from "./aboutStore";
 import ProjectStore from "./projectStore";
 import BlogPostStore from "./blogPostStore";
+import LoginStore from "./loginStore";
+import CommonStore from "./commonStore";
+
 // Define the store
 interface Store {
     aboutStore: AboutStore,
     projectStore: ProjectStore,
-    blogPostStore: BlogPostStore
+    blogPostStore: BlogPostStore,
+    loginStore: LoginStore,
+    commonStore: CommonStore
 }
 
 // We instantiate our stores here
 export const store: Store = {
     aboutStore: new AboutStore(),
     projectStore: new ProjectStore(),
-    blogPostStore: new BlogPostStore()
+    blogPostStore: new BlogPostStore(),
+    loginStore: new LoginStore(),
+    commonStore: new CommonStore()
 }
 
 // We create the react context of the store so its available to use between components

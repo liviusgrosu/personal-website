b817a333511a1cc5d2574228a3cde6cf7e71468d
liviusgrosu <liviusgrosu@gmail.com>
2024-06-03 16:31:16 -0600

    Added two buttons to each project card; delete and edit

--- client-app/src/features/ProjectCard.tsx
+++ client-app/src/features/ProjectCard.tsx
@@ -1,20 +1,49 @@
 /* eslint-disable react-refresh/only-export-components */
-import {Card, Image} from "semantic-ui-react";
+import {Button, Card, Image} from "semantic-ui-react";
 import {observer} from "mobx-react-lite";
 import {Link} from "react-router-dom";
 import { Project } from "../app/models/project";
+import { useStore } from "../app/stores/store";
+import { useEffect } from "react";
 
 interface Props {
     project: Project
 }
 
 export default observer(function ProfileCard({project}: Props) {
+    const {loginStore} = useStore();
+    const {user} = loginStore; 
+
+    const buttonAction = (action: string) => {
+        console.log(action);
+    };
+
     return (
-        <Card as={Link} to={`/projects/${project.id}`}>
-            <Image src={project.image || '/placeholder.png'} />
-            <Card.Content>
-                <Card.Header>{project.title}</Card.Header>
-            </Card.Content>
+        <Card>
+            <Link to={`/projects/${project.id}`} style={{ display: 'block', marginBottom: '1em' }}>
+                <Image src={project.image || '/placeholder.png'} />
+                <Card.Content>
+                    <Card.Header>{project.title}</Card.Header>
+                </Card.Content>
+            </Link>
+            {user !== null && (
+                <Card.Content extra>
+                    <div className="ui two buttons">
+                        <Button 
+                            basic color="blue"
+                            onClick={() => buttonAction('edit')}
+                        >
+                        Edit
+                        </Button>
+                        <Button 
+                            basic color="red"
+                            onClick={() => buttonAction('delete')}
+                        >
+                            Delete
+                        </Button>
+                    </div>
+                </Card.Content>
+            )}
         </Card>
-    )
+    );
 })

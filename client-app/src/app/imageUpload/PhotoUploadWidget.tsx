adbbde0b5ac992b3e1db4c81c51819c61b8a850e
liviusgrosu <liviusgrosu@gmail.com>
2024-06-05 20:36:34 -0600

    Added button to open modal to edit picture

---
+++ client-app/src/app/imageUpload/PhotoUploadWidget.tsx
@@ -0,0 +1,59 @@
+import { Button, Grid, GridColumn, Header } from "semantic-ui-react";
+import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
+import { useEffect, useState } from "react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
+ 
+interface Props {
+    uploadPhoto: (file: Blob) => void;
+}
+
+export default function PhotoUploadWidget({uploadPhoto}: Props) {
+    const [files, setFiles] = useState<any>([]);
+    const [cropper, setCropper] = useState<Cropper>();
+
+    function onCrop() {
+        if (cropper) {
+            cropper.getCroppedCanvas().toBlob(blob => uploadPhoto(blob!));
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            files.forEach((file: object & {preview?: string}) => 
+                URL.revokeObjectURL(file.preview!));
+        }
+    }, [files])
+
+    return (
+         <Grid>
+            <Grid.Row>
+                <Grid.Column width={16}>
+                    <Header sub content='Step 1 - Add Photo'/>
+                    <PhotoWidgetDropzone setFiles={setFiles}/>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column width={16}>
+                    <Header sub content='Step 2 - Resize Image'/>
+                    {files && files.length > 0 && (
+                        <PhotoWidgetCropper setCropper={setCropper} imagePreview={files[0].preview}/>
+                    )}
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column width={16}>
+                    <Header sub content='Step 3 - Preview & Upload'/>
+                    {files && files.length > 0 &&
+                        <>
+                            <div className='img-preview' style={{minHeight: 100, overflow: 'hidden'}}/>
+                            <Button.Group widths={2}>
+                                <Button onClick={onCrop} positive icon='check'/>
+                                <Button onClick={() => setFiles([])} icon='close'/>
+                            </Button.Group>
+                        </>
+                    }
+                </Grid.Column>
+            </Grid.Row>
+         </Grid>
+    )
+}
--- client-app/src/features/ProjectCard.tsx
+++ client-app/src/features/ProjectCard.tsx
@@ -1,43 +1,56 @@
 /* eslint-disable react-refresh/only-export-components */
-import {Button, Card, Image} from "semantic-ui-react";
+import {Button, Card, Icon, Image} from "semantic-ui-react";
 import {observer} from "mobx-react-lite";
 import {Link} from "react-router-dom";
 import { Project } from "../app/models/project";
 import { useStore } from "../app/stores/store";
-import { useEffect } from "react";
+import PhotoUploadWidget from "../app/imageUpload/PhotoUploadWidget";
 
 interface Props {
     project: Project
 }
 
 export default observer(function ProfileCard({project}: Props) {
-    const {loginStore} = useStore();
-    const {user} = loginStore; 
+    const {commonStore, modalStore} = useStore();
+    const {token} = commonStore; 
 
-    const buttonAction = (action: string) => {
-        console.log(action);
-    };
+    function handlePhotoUpload(file: Blob) {
+        console.log("file uploading = " + file);
+    }
 
     return (
         <Card>
-            <Link to={`/projects/${project.id}`} style={{ display: 'block', marginBottom: '1em' }}>
-                <Image src={project.image || '/placeholder.png'} />
-                <Card.Content>
-                    <Card.Header>{project.title}</Card.Header>
-                </Card.Content>
-            </Link>
-            {user !== null && (
+            <div style={{ position: 'relative' }}>
+                <Link to={`/projects/${project.id}`} style={{ display: 'block', marginBottom: '1em' }}>
+                    <Image src={project.image || '/placeholder.png'} />
+                    <Card.Content>
+                        <Card.Header>{project.title}</Card.Header>
+                    </Card.Content>
+                </Link>
+                {token !== null && (
+                    <Button 
+                        icon="pencil icon"
+                        basic 
+                        color="blue" 
+                        style={{ position: 'absolute', top: '10px', right: '10px', zIndex: 1 }}
+                        onClick={(e) => {e.stopPropagation(); modalStore.openModal(<PhotoUploadWidget uploadPhoto={handlePhotoUpload}/>)}} // Prevents triggering the link
+                    />
+                )}
+            </div>
+
+            {token !== null && (
                 <Card.Content extra>
                     <div className="ui two buttons">
                         <Button 
                             basic color="blue"
-                            onClick={() => buttonAction('edit')}
+                            as={Link}
+                            to={`/projects-edit/${project.id}`}
                         >
-                        Edit
+                            Edit
                         </Button>
                         <Button 
                             basic color="red"
-                            onClick={() => buttonAction('delete')}
+                            
                         >
                             Delete
                         </Button>

8bc6f8c9c8b0f7879691409ab3861c2646605e3e
liviusgrosu <liviusgrosu@gmail.com>
2024-07-22 19:37:45 -0600

    Passed cover photo to blog post list

--- PersonalWebsite/Application/Blog/BlogPostDto.cs
+++ PersonalWebsite/Application/Blog/BlogPostDto.cs
@@ -11,6 +11,7 @@ namespace Application.Blog
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
         public DateTime Date { get; set; }
     }
 }

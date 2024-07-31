83bd1fb2282fc2dc87a5cdf3b5e7845350063b58
liviusgrosu <liviusgrosu@gmail.com>
2024-07-30 20:46:34 -0400

    Adjusted styling for iframe test in chrome

--- client-app/src/features/ProjectDetail.tsx
+++ client-app/src/features/ProjectDetail.tsx
@@ -6,6 +6,9 @@ import { useEffect } from "react";
 import { Button, ButtonGroup, Header, Label } from "semantic-ui-react";
 import PhotoUploadWidget from "../app/imageUpload/PhotoUploadWidget";
 import ProjectDelete from "./ProjectDelete";
+import ResizableIframe from "../app/common/ResizableIframe";
+import FitIframes from "../app/common/FitIframes";
+import IframeResizer from "@iframe-resizer/react";
 
 export default observer(function ProjectDetail() {
     const navigate = useNavigate();
@@ -77,6 +80,11 @@ export default observer(function ProjectDetail() {
                             <Label content={tag} key={tag} style={{ margin: '5px' }}/>
                         ))}
                     </div>
+
+                    <div className="videoContainer">
+                        <iframe  className="responsive-iframe" src="https://user-images.githubusercontent.com/1690449/119239331-bf6df700-bb1e-11eb-87d7-678d8ba15065.mp4"></iframe>
+                    </div> 
+
                     <div className="detailsPage" dangerouslySetInnerHTML={{__html: selectedProjectDetails.content}}/>
                 </>
             )}            

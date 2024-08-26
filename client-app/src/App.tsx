04d20744ba72ce1af76e14e177f61279a5596f5c
liviusgrosuMO <lgrosu@metocean.com>
2024-08-26 12:21:25 -0400

    Cleaned up app.tsx

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { Container, DimmerDimmable, Header, Segment } from "semantic-ui-react";
+import { Container, Segment } from "semantic-ui-react";
 import NavBar from "./navbar/NavBar";
 import { Outlet } from "react-router-dom";
 import { observer } from "mobx-react-lite";
@@ -14,9 +14,6 @@ export default observer(function App() {
 
       <ModalContainer/>
       <Container as="nav">
-        {/* <Header inverted as="h1">
-            Livius Grosu
-        </Header> */}
         <NavBar/>
       </Container>
       <Container 
